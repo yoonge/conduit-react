3a613157f8bcbffe87763950408eef00237de7b2
Yoonge <7884008@qq.com>
2023-06-17 23:18:44 +0800

    Import react-router-dom and ant-design/pro-components.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
 import './index.less'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
