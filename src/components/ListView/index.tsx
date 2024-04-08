c78999b3d23b980981c957ebb96ed7d873c10641
Yoonge <7884008@qq.com>
2024-04-08 23:36:33 +0800

    refactor: :recycle: refactor `ListItem` component
    Co-authored-by: Svelte <dev.yoonge@gmail.com>

--- src/components/ListView/index.tsx
+++ src/components/ListView/index.tsx
@@ -24,31 +24,29 @@ const ListView: React.FC<ListViewProps> = ({ activeKey, handleTabSelect }) => {
   }
 
   return (
-    <section>
-      <div className="topicList list-group">
-        {topicList.length ? (
-          topicList.map(item => <ListItem key={item?._id} topic={item} />)
-        ) : (
-          <div className="py-4 text-muted">
-            {['favorites', 'my-favorites'].includes(activeKey) && (
-              <>
-                Nothing yet... You can find some topics that interest you <a href="" onClick={handleClick}>HERE</a>.
-              </>
-            )}
-            {['all', 'my-topics', 'topics'].includes(activeKey) && (
-              <>
-                Nothing yet... You can initiate a new topic <a href="/topic/initiate">HERE</a>.
-              </>
-            )}
-            {(activeKey.indexOf('tag') === 0) && (
-              <>
-                Nothing yet... You can initiate a new topic with <Badge bg="success">{activeKey.substring(3)}</Badge> <a href="/topic/initiate">HERE</a>.
-              </>
-            )}
-          </div>
-        )}
-      </div>
-    </section>
+    <div className="topicList list-group">
+      {topicList.length ? (
+        topicList.map(item => <ListItem key={item?._id} topic={item} />)
+      ) : (
+        <div className="py-4 text-muted">
+          {['favorites', 'my-favorites'].includes(activeKey) && (
+            <>
+              Nothing yet... You can find some topics that interest you <a href="" onClick={handleClick}>HERE</a>.
+            </>
+          )}
+          {['all', 'my-topics', 'topics'].includes(activeKey) && (
+            <>
+              Nothing yet... You can initiate a new topic <a href="/topic/initiate">HERE</a>.
+            </>
+          )}
+          {(activeKey.indexOf('tag') === 0) && (
+            <>
+              Nothing yet... You can initiate a new topic with <Badge bg="success">{activeKey.substring(3)}</Badge> <a href="/topic/initiate">HERE</a>.
+            </>
+          )}
+        </div>
+      )}
+    </div>
   )
 }
 
